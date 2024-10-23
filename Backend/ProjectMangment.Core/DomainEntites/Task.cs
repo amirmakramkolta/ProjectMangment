6b6e01bfe5bf51c27679b2ab62f55704f50a1659
Amir Makram <35640764+amirmakramkolta@users.noreply.github.com>
2024-10-23 22:40:41 +0300

    Add other enties

---
+++ Backend/ProjectMangment.Core/DomainEntites/Task.cs
@@ -0,0 +1,20 @@
+﻿using ProjectMangment.Core.DomainEntites.Enums;
+
+namespace ProjectMangment.Core.DomainEntites
+{
+    public class Task
+    {
+        private Task()
+        {
+            
+        }
+        public Guid TaskId { get; private set; }
+        public string TaskName { get; private set; }
+        public string Description { get; private set; }
+        public int AssignedToId { get; private set; }
+        public DateOnly StartDate { get; private set; }
+        public DateOnly EndDate { get; private set; }
+        public int Priority { get; private set; }
+        public Status Status { get; private set; }
+    }
+}
---
+++ Backend/ProjectMangment.Core/DomainEntites/User.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectMangment.Core.DomainEntites
+{
+    public class User
+    {
+        private User()
+        {
+            
+        }
+        public Guid UserId { get; private set; }
+        public string Name { get; private set; }
+    }
+}
